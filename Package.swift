29175af7070af197c313dcf242d87b876acd64e1
Joseph Kohlmann <joseph.kohlmann@nytimes.com>
2019-03-12 17:08:15 -0400

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Zip",
+    products: [
+        .library(name: "Zip", targets: ["Zip"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Zip",
+            dependencies: []),
+    ]
+)
